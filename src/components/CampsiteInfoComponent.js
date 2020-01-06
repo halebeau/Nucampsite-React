66d44ea422a0d7ba853380e763beb2c9536002e2
Beau Hale <halebeau3@gmail.com>
2020-01-05 20:26:07 -0800

    finishing components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,57 @@
 import React, { Component } from "react";
+import Reactstrap, {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle
+} from "reactstrap";
 
 class CampsiteInfo extends Component {
+  renderCampsite(campsite) {
+    return (
+      <div className="col-md-5 m-1">
+        <Card>
+          <CardImg top src={campsite.image} alt={campsite.name} />
+          <CardBody>
+            <CardTitle>{campsite.name}</CardTitle>
+            <CardText>{campsite.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
   render() {
     if (this.props.campsite) {
-      return <div className="row" />;
+      return this.campsite.renderCampsite;
+    }
+    return <div />;
+  }
+
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <div className="md-col-5 m-1">
+          <h4>Comments {comments}</h4>
+          {comments.map(comments => {
+            return (
+              <div key={comments.text}>
+                <div>
+                  {comments.author},{" "}
+                  {new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit"
+                  }).format(new Date(Date.parse(comments.date)))}
+                </div>
+                <div>{comments.date}</div>
+              </div>
+            );
+          })}
+        </div>
+      );
     }
     return <div />;
   }
