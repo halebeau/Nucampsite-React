2d9e8de861598d5848632d05b7caa2ba9c74558b
Beau Hale <halebeau3@gmail.com>
2020-01-25 00:12:30 -0800

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -25,7 +25,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="md-col-5 m-1">
@@ -46,7 +46,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={ campsiteId } postComment={ postComment} />
       </div>
     );
   }
@@ -69,7 +69,7 @@ class CommentForm extends Component {
   }
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(
+    this.props.postComment(
       this.props.campsiteId,
       values.ratings,
       values.author,
@@ -191,7 +191,7 @@ function CampsiteInfo(props) {
           <RenderCampsite campsite={props.campsite} />
           <RenderComments
             comments={props.comments}
-            addComments={props.addComments}
+            postComment={props.postComment}
             campsiteId={props.campsite.id}
           />
         </div>
